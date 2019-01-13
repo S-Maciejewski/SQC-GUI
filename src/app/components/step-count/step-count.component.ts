4676a633d2e205e60366a11178b84a37ccd70715
S-Maciejewski <sebastian_maciejewski@yahoo.com>
2019-01-13 12:24:13 +0100

    Step count component

---
+++ src/app/components/step-count/step-count.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-step-count',
+  templateUrl: './step-count.component.html',
+  styleUrls: ['./step-count.component.scss']
+})
+export class StepCountComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
